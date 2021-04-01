1b2a53104e5b5c4cc9f2fc2e9c55ec3f9c7515a0
Nikhil Reddy Pannala <77602266+Nikhilreddy012@users.noreply.github.com>
2021-03-31 21:41:55 -0500

    Created slot route

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var newspapersRouter = require('./routes/newspapers');
 var starsRouter = require('./routes/stars');
+var slotRouter = require('./routes/slot');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/newspapers', newspapersRouter);
 app.use('/stars', starsRouter);
+app.use('/slot', slotRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
